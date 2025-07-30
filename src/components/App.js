23338dd9e70df017371fb35a765d81be88cd4dcf
Nishar24 <mdnishar75640@gmail.com>
2025-07-30 20:25:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,14 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <Routes>
+      <Route path="/" element={<ProfileForm />} />
+      <Route path="/education" element={<EducationForm />} />
+      <Route path="/skills" element={<SkillsForm />} />
+      <Route path="/projects" element={<ProjectsForm />} />
+      <Route path="/social" element={<SocialForm />} />
+      <Route path="/resume" element={<ResumePreview />} />
+    </Routes>
     </div>
   )
 }
